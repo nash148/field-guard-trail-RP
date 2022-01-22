fb6c9db69f111ee54e323b692c1aa930ef9eb2e8
nash148 <natansheena@gmail.com>
2022-01-22 22:50:47 +0200

    Fixed bug in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,25 +5,25 @@ echo "Starting setup..."
 MAIN_DIR="~/field-guard"
 
 # Create the project main directory
-mkdir $MAIN_DIR
+mkdir ~/field-guard
 echo "Created $MAIN_DIR dir"
 
 # Create temp directory for the images
-mkdir $MAIN_DIR/temp-images
+mkdir ~/field-guard/temp-images
 echo "Created $MAIN_DIR/temp-images dir"
 
 # Crate scripts dir 
-mkdir $MAIN_DIR/scripts
+mkdir ~/field-guard/scripts
 echo "Created $MAIN_DIR/scripts dir"
 
 # Copy the relevant scripts the the main directory
-cp ./run_power_supply_main.sh $MAIN_DIR/scripts
-cp ./run_upload_pictures_main.sh $MAIN_DIR/scripts
+cp ./run_power_supply_main.sh ~/field-guard/scripts
+cp ./run_upload_pictures_main.sh ~/field-guard/scripts
 echo "Copied the relevant scripts"
 
 # Change the permissions of the scripts
-sudo chmod +x $MAIN_DIR/scripts/run_power_supply_main.sh
-sudo chmod +x $MAIN_DIR/scripts/run_upload_pictures_main.sh
+sudo chmod +x ~/field-guard/scripts/run_power_supply_main.sh
+sudo chmod +x ~/field-guard/scripts/run_upload_pictures_main.sh
 echo "Changed the scripts poermissions"
 
 # Setup the power supply service
