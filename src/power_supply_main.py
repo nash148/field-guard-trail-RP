8da341c53477422a5474511655839bf86eee785a
nash148 <natansheena@gmail.com>
2022-01-22 22:06:03 +0200

    Added main script for power supply only

---
+++ src/power_supply_main.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+# Set the working directory as PYTHONPATH
+sys.path.append(os.getcwd())
+
+from src.control_manager.ControlManager import ControlManager
+from src.cloud_handler.DropboxHandler import DropboxHandler
+from src.logger.Logger import MyLogger
+
+logger = MyLogger()
+
+
+def main():
+    logger.info('#################################')
+    logger.info('##  Starting power main...  ##')
+    logger.info('#################################')
+
+    cloud_handler = DropboxHandler()
+    control_manager = ControlManager(cloud_handler)
+    control_manager.start_power_supply()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        logger.error(str(e))
