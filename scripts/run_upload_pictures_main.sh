96359b7d6ed327b3b593639234967d6a6ad7a4cf
nash148 <nash148@github.com>
2022-02-13 15:00:39 +0000

    Added waiting to internet before upload

--- scripts/run_upload_pictures_main.sh
+++ scripts/run_upload_pictures_main.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+while ! ping -c 1 -W 1 8.8.8.8; do
+	echo "Waiting for internet"
+	sleep 1
+done
+
+#sudo systemctl start mnt-volume.automount
+
 cd /home/pi/clones/field-guard-trail-RP
-sudo python ./src/upload_pictures_main.py
+sudo python ./src/upload_pictures_main.py > /dev/tty1
