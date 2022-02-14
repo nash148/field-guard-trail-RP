0b08939d11233dcb1ad3a09d41d90f950e281606
natan <natansheena@gmail.com>
2022-02-15 00:14:27 +0200

    Create FilesHandler class

--- src/control_manager/ControlManager.py
+++ src/control_manager/ControlManager.py
@@ -1,59 +1,40 @@
 import datetime
 import os
 from time import sleep
+from typing import Union
 
 from src.logger.Logger import MyLogger
 from src.cloud_handler.CloudHandlerProtocol import CloudHandlerProtocol
-from src.RPi_handler.RPHandler import GPIOHandler
-from src.config.settings import camera_conf, files_conf
-from src.utils.files_utils import move_files_from_cam_ro_rpi, delete_pictures_from_rpi
+from src.gpio_handler.GpioHandler import GpioHandler
+from src.files_handler.FilesHandler import FilesHandler
 from src.utils.rpi_utils import shutdown_rpi
 
-
-def run_mount_command():
-    cmd = 'sudo mount /dev/sda1 /mnt/volume'
-    os.system(cmd)
 
 class ControlManager:
     """
     The bug manager
     """
-    def __init__(self, cloud_handler: CloudHandlerProtocol):
+    def __init__(self):
         """Initial the attributes"""
-        self._cloud_handler = cloud_handler
         self._logger = MyLogger()
-        self._rpi_handler = GPIOHandler()
+        self._gpio_handler = GpioHandler()
+        self._files_handler = FilesHandler()
         self._logger.info('Init ControlManager class')
 
-    def upload_camera_pictures(self, shutdown: bool = False):
+    def upload_camera_pictures(self, cloud_handler: CloudHandlerProtocol, shutdown: bool = False):
         """Upload the pictures from the camera to the cloud"""
 
-        now = datetime.datetime.now()
-        timestamp = now.strftime("%d-%m-%Y_%H-%M-%S")
-
-        # Open usb socket
-        self._rpi_handler.open_usb_socket()
-
-        sleep(7)
-        self._logger.info('Run mount command')
-        run_mount_command()
-
-        sleep(2)
-
-        # Move the pictures from camera to the RPi
-        move_files_from_cam_ro_rpi()
-
-        # Close usb socket
-        self._rpi_handler.close_usb_socket()
+        # Move the images from the camera to the RPi
+        self._files_handler.move_files_from_cam_to_rpi()
 
         # Reset camera
-        self._rpi_handler.reset_camera()
+        self._gpio_handler.reset_camera()
 
         # Upload the pictures to the cloud
-        self._upload_pics_to_cloud(timestamp)
+        self._files_handler.upload_images_to_cloud(cloud_handler)
 
         # Delete pictures from RPi storage
-        delete_pictures_from_rpi()
+        self._files_handler.delete_images_from_rpi()
 
         if shutdown:
             # Shutdown the RPi
@@ -61,25 +42,11 @@ class ControlManager:
 
     def start_power_supply(self, time_to_supply: int = None):
         """Start power supply"""
-        self._rpi_handler.start_power_supply()
+        self._gpio_handler.start_power_supply()
         
         if time_to_supply is not None:
             sleep(time_to_supply)
-            self._rpi_handler.stop_power_supply()
+            self._gpio_handler.stop_power_supply()
         else:  # Run forever
             while True:
                 sleep(1)
-
-    def _upload_pics_to_cloud(self, start_timestamp: str):
-        """Upload every picture in the pictures folder"""
-
-        # Create new folder in the cloud
-        self._cloud_handler.create_folder(files_conf['remote_pics_path'] + start_timestamp)
-
-        rpi_pics_path = files_conf['RPi_pics_path']
-
-        # Run over every file in the pictures folder
-        for curr_file in os.listdir(files_conf['RPi_pics_path']):
-            self._cloud_handler.upload_file(
-                rpi_pics_path + '/' + curr_file,
-                files_conf['remote_pics_path'] + start_timestamp + '/' + curr_file)
